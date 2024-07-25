27a92a585a4ed6db49f7d4ce03e6d66e560c82d2
ToanHD <toanhanduc@gmail.com>
2024-07-25 22:51:13 +0700

    Update Templating Engine and Clean Up HTML Files
    - server.js:
      - Set EJS as the templating engine and configure views directory (lines 6-9).
      - Update routes to render EJS files instead of sending HTML files (lines 12-33).
    
    - views/contact.html:
      - Removed the entire contact form section to streamline the page and remove unnecessary content.
    
    - views/index.html:
      - Removed all content to clean up unnecessary code and improve readability.
    
    - views/projects.html:
      - Removed the Projects Section to improve the layout and content organization.
    
    - Removed views/resume.html and its associated content from the project.
    
    - package-lock.json:
      - Added ejs dependency version 3.1.10.
      - Added async dependency version 3.2.5.
      - Added ansi-styles and chalk dependencies.
      - Updated supports-color dependency version to 7.2.0.
      - Updated color-convert dependency version to 2.0.1.
    
    - package.json:
      - Updated Express dependency to version 4.19.2.
      - Added EJS dependency version 3.1.10.

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
 // Route to serve the contact.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.render("index");
 });
 
 app.get("/projects", (req, res) => {
-  res.sendFile(path.join(__dirname, "projects.html"));
+  res.render("projects");
 });
+
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "contact.html"));
+  res.render("contact");
 });
 
 app.get("/resume", (req, res) => {
-  res.sendFile(path.join(__dirname, "resume.html"));
+  res.render("resume");
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
